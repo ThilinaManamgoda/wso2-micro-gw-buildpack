e20b9e19812df6f638e5cacceef6b4a033b6115e
ThilinaManamgoda <thilinama@wso2.com>
2019-06-06 19:52:35 +0530

    Remove .sh extention files

--- bin/detect.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Application root directory should contain the configuration file "wso2-am-gw"
-APP_DIR=${1}
-if [[ -f "${APP_DIR}/wso2-am-gw" ]]; then
-  exit 0
-fi
-
-exit 1
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,54 @@
 #!/usr/bin/env bash
+
+set -e
+
+WSO2_AM_GW_VERSION="2.6.0"
+JDK_VERSION="1.8.0_201"
+CERTIFICATE="ixdevwso2com.crt"
+
+BUILD_SCRIPT_DIR=$(dirname $(dirname $0))
+
+echo ${BUILD_SCRIPT_DIR}
+if [[ ! -f "${BUILD_SCRIPT_DIR}/../resources/openjdk-${JDK_VERSION}.tar.gz" ]]; then
+    echo "${BUILD_SCRIPT_DIR}/../resources/openjdk-${JDK_VERSION}.tar.gz is not available"
+    exit 1
+fi
+
+if [[ ! -f "${BUILD_SCRIPT_DIR}/../resources/wso2am-micro-gw-${WSO2_AM_GW_VERSION}.zip" ]]; then
+    echo "${BUILD_SCRIPT_DIR}/../resources/wso2am-micro-gw-${WSO2_AM_GW_VERSION}.zip is not available"
+    exit 1
+fi
+
+if ! unzip ${BUILD_SCRIPT_DIR}/../resources/wso2am-micro-gw-${WSO2_AM_GW_VERSION}.zip; then
+    echo "couldn't unzip the ${BUILD_SCRIPT_DIR}/../resources/wso2am-micro-gw-${WSO2_AM_GW_VERSION}.zip"
+    exit 1
+fi
+
+if [[ -f "${BUILD_SCRIPT_DIR}/../resources/${CERTIFICATE}" ]]; then
+ keytool -import -alias cf-am-micro-gw -keystore wso2am-micro-gw-${WSO2_AM_GW_VERSION}/lib/platform/bre/security/ballerinaTruststore.p12 -file ${BUILD_SCRIPT_DIR}/../resources/${CERTIFICATE} -storepass ballerina  -noprompt
+ else
+    echo "${BUILD_SCRIPT_DIR}/../resources/${CERTIFICATE} is not available"
+    exit 1
+fi
+
+if ! zip -r wso2am-micro-gw-${WSO2_AM_GW_VERSION}.zip wso2am-micro-gw-${WSO2_AM_GW_VERSION}/*;
+ then
+    echo "couldn't package wso2am-micro-gw-${WSO2_AM_GW_VERSION}/* to wso2am-micro-gw-${WSO2_AM_GW_VERSION}.zip"
+    exit 1
+fi
+
+if ! cp wso2am-micro-gw-${WSO2_AM_GW_VERSION}.zip ${BUILD_SCRIPT_DIR}/../resources; then
+    echo "couldn't copy wso2am-micro-gw-${WSO2_AM_GW_VERSION}.zip to ${BUILD_SCRIPT_DIR}/../resources"
+    exit 1
+fi
+
+if ! rm -rf wso2am-micro-gw-${WSO2_AM_GW_VERSION}; then
+    echo "couldn't remove wso2am-micro-gw-${WSO2_AM_GW_VERSION}"
+    exit 1
+fi
+
+if ! rm -rf wso2am-micro-gw-${WSO2_AM_GW_VERSION}.zip; then
+    echo "couldn't remove wso2am-micro-gw-${WSO2_AM_GW_VERSION}.zip"
+    exit 1
+fi
+
