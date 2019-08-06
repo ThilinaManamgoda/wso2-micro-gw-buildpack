da350ef63f44a3e9e56b85977dd38eee346e7d33
maanadev <maanafunedu@gmail.com>
2019-08-06 22:03:21 +0530

    Add JWT cert via build script

--- micro-gw-buildpack/scripts/build.sh
+++ micro-gw-buildpack/scripts/build.sh
@@ -54,6 +54,8 @@ if ! unzip -q ${BUILD_SCRIPT_DIR}/../dist/wso2am-micro-gw-linux-${WSO2_AM_GW_RUN
     echo_error_exit "Couldn't unzip the ${BUILD_SCRIPT_DIR}/../dist/wso2am-micro-gw-linux-${WSO2_AM_GW_RUNTIME_VERSION}.zip"
 fi
 
+cp ${BUILD_SCRIPT_DIR}/../dist/conf/micro-gw.conf ${BUILD_SCRIPT_DIR}/wso2am-micro-gw-linux-${WSO2_AM_GW_RUNTIME_VERSION}/conf/
+
 pushd ${BUILD_SCRIPT_DIR}/wso2am-micro-gw-linux-${WSO2_AM_GW_RUNTIME_VERSION}
     if ! unzip -q runtime.zip -d runtime;
      then
@@ -74,6 +76,10 @@ do
     echo_error_exit "Couldn't import the certificate: ${cert_file}"
   fi
 done
+
+keytool -delete -noprompt -alias wso2apim  -keystore ${BUILD_SCRIPT_DIR}/wso2am-micro-gw-linux-${WSO2_AM_GW_RUNTIME_VERSION}/runtime/bre/security/ballerinaTruststore.p12 -storepass ${TRUST_STORE_PASSWORD}
+keytool -import -alias wso2apim -keystore ${BUILD_SCRIPT_DIR}/wso2am-micro-gw-linux-${WSO2_AM_GW_RUNTIME_VERSION}/runtime/bre/security/ballerinaTruststore.p12 -file ${BUILD_SCRIPT_DIR}/../dist/jwt_cert/jwt.crt -storepass ${TRUST_STORE_PASSWORD}  -noprompt
+
 
 pushd ${BUILD_SCRIPT_DIR}/wso2am-micro-gw-linux-${WSO2_AM_GW_RUNTIME_VERSION}/runtime
     if ! zip -q -r runtime.zip .; then
@@ -108,7 +114,10 @@ if ! rm  ${BUILD_SCRIPT_DIR}/wso2am-micro-gw-linux-${WSO2_AM_GW_RUNTIME_VERSION}
 fi
 
 pushd ${BUILD_SCRIPT_DIR}/../
- if ! buildpack-packager build  -any-stack -cached; then
+ if ! buildpack-packager build  -any-stack; then
   echo_error_exit "Couldn't build the Build pack"
  fi
 popd
+#
+#cf delete-buildpack -f wso2am-micro-gw
+#cf create-buildpack wso2am-micro-gw /Users/wso2/projects/ix/pcf/buildpack-project/buildpack-m3/micro-gw-buildpack/wso2am-micro-gw_buildpack-v1.0.0.zip  0 --enable
